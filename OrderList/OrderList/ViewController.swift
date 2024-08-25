7deee6ffe38ce22adeeb716bdd377b0b519254c3
DrakeYang <drake.the.programmer@gmail.com>
2024-08-25 14:57:38 +0900

    메인뷰컨에 로그인뷰 출력: 앱 실행시 로그인뷰 실행

--- OrderList/OrderList/ViewController.swift
+++ OrderList/OrderList/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.loadLoginView()        
+        self.setupKeyboardObservers()
+        self.setupTapGesture()
     }
-
+    
+    func loadLoginView(){
+        let loginView = LoginView()
+        self.view.addSubview(loginView)
+        loginView.setupLayout()
+        loginView.snp.makeConstraints { (make) -> Void in
+            make.width.height.equalTo(self.view.safeAreaLayoutGuide)
+            make.center.equalTo(self.view)
+        }
+    }
+    
 
 }
-
