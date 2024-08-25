d75e8536203910b99278d8d2a1deba3aabae81a0
DrakeYang <drake.the.programmer@gmail.com>
2024-08-25 16:43:30 +0900

    주문목록 vc 파일 및 내용 추가

---
+++ OrderList/OrderList/OrderListVC.swift
@@ -0,0 +1,28 @@
+//
+//  OrderListVC.swift
+//  OrderList
+//
+//  Created by YongMo Yang on 2024/08/25.
+//
+
+import Foundation
+import UIKit
+
+class OrderListVC: UIViewController {
+    override func viewDidLoad() {
+        self.makeTitleView()
+        self.view.backgroundColor = .white
+    }
+    
+    // 상단뷰 만들기
+    func makeTitleView(){
+        let titleView = UIView()
+        self.view.addSubview(titleView)
+        titleView.backgroundColor = .blue
+        titleView.snp.makeConstraints { (make) -> Void in
+            make.height.equalTo(50)
+            make.width.equalTo(self.view)
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
+}
